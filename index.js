bdc9b18c68fefc61f83df5048c52b9fe0ab2939e
Johannes <johannes.viirimaki@gmail.com>
2023-02-09 16:57:17 +0200

    ready for render

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { json } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('data', function(req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
@@ -85,7 +87,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
